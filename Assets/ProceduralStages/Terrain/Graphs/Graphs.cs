73fe12552e07de7c1d8506be0666f228428e6399
Olivier Lavoie <olivier.lavoie@nosotech.com>
2024-02-24 09:25:03 -0500

    More restructuting

---
+++ Assets/ProceduralStages/Terrain/Graphs/Graphs.cs
@@ -0,0 +1,38 @@
+﻿using RoR2.Navigation;
+using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ProceduralStages
+{
+    public class Graphs
+    {
+        public NodeGraph ground;
+        public NodeGraph air;
+        public List<PropsNode> floorProps;
+        public List<PropsNode> ceilingProps;
+
+        public Dictionary<Vector3, PropsNode> nodeInfoByPosition;
+        public Dictionary<Vector3, int> groundNodeIndexByPosition;
+
+        public void OccupySpace(Vector3 position, bool solid)
+        {
+            if (groundNodeIndexByPosition.TryGetValue(position, out int index))
+            {
+                ref var node = ref ground.nodes[index];
+
+                node.flags = NodeFlags.NoCharacterSpawn | NodeFlags.NoShrineSpawn | NodeFlags.NoChestSpawn;
+
+                if (solid)
+                {
+                    node.flags |= NodeFlags.NoCharacterSpawn;
+                    node.forbiddenHulls = HullMask.Human | HullMask.Golem | HullMask.BeetleQueen;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/ProceduralStages/Terrain/Graphs/PropsNode.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ProceduralStages
+{
+    public struct PropsNode
+    {
+        public Vector3 position;
+        public Vector3 normal;
+
+        public GameObject Place(
+            GameObject prefab,
+            GameObject parent,
+            Material material,
+            Color? color,
+            Vector3? normal,
+            float scale)
+        {
+            var rotation = Quaternion.FromToRotation(Vector3.up, normal ?? this.normal)
+                * prefab.transform.rotation;
+
+            GameObject gameObject = GameObject.Instantiate(prefab, position, rotation, parent.transform);
+
+            gameObject.transform.Rotate(normal ?? this.normal, MapGenerator.rng.nextNormalizedFloat * 360f, Space.World);
+            gameObject.transform.localScale = new Vector3(scale, scale, scale);
+
+            if (Application.isEditor)
+            {
+                LODGroup[] lodGroups = gameObject.GetComponentsInChildren<LODGroup>();
+                foreach (LODGroup lodGroup in lodGroups)
+                {
+                    var lods = lodGroup.GetLODs();
+
+                    lods[lods.Length - 1].screenRelativeTransitionHeight = 0;
+                    lodGroup.SetLODs(lods);
+                }
+            }
+
+            if (material != null)
+            {
+                var meshRenderers = gameObject.transform.GetComponentsInChildren<MeshRenderer>();
+                for (int i = 0; i < meshRenderers.Length; i++)
+                {
+                    meshRenderers[i].material = material;
+                }
+            }
+
+            if (color.HasValue)
+            {
+                var meshRenderers = gameObject.transform.GetComponentsInChildren<MeshRenderer>();
+                for (int i = 0; i < meshRenderers.Length; i++)
+                {
+                    foreach (var m in meshRenderers[i].materials)
+                    {
+                        m.SetColor("_Color", color.Value);
+                    }
+                }
+            }
+
+            //gameObject.transform.Rotate(Vector3.up, MapGenerator.rng.RangeFloat(0.0f, 360f), Space.Self);
+            return gameObject;
+        }
+    }
+}
