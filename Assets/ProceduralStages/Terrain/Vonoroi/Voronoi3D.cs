e86e41cf1b85d152f9a0606d6fc6e99353252038
Olivier Lavoie <olivier.lavoie@nosotech.com>
2024-06-24 12:04:00 -0400

    Chebyshev expirement

--- Assets/ProceduralStages/Terrain/Vonoroi/Voronoi3D.cs
+++ Assets/ProceduralStages/Terrain/Vonoroi/Voronoi3D.cs
@@ -12,13 +12,14 @@ namespace ProceduralStages
     {
         public Vector3Int size;
         public Vector3Int cellCounts;
+        public Metric metric = Metric.Euclidean;
 
         [SerializeField]
         [HideInInspector]
         private Voronoi3DResult[] voronoi;
 
-        [ContextMenu("Bake")]
-        public void Bake()
+        [ContextMenu("BakeEuclidean")]
+        public void BakeEuclidean()
         {
             voronoi = new Voronoi3DResult[size.x * size.y * size.z];
 
@@ -71,6 +72,7 @@ namespace ProceduralStages
 
                                     Vector3 displacement = RandomPG.Random3(clampedPos);
                                     Vector3 diff = neighbor + displacement - uvwFractional;
+
                                     float dist = diff.sqrMagnitude;
                                     if (dist < minDistance1)
                                     {
@@ -99,16 +101,207 @@ namespace ProceduralStages
                             scaleReciprocal.y * minDistanceDisplacement2.y,
                             scaleReciprocal.z * minDistanceDisplacement2.z);
 
-                        //https://www.youtube.com/watch?v=PMltMdi1Wzg
+                        
                         Vector3 pa = -minDistanceDisplacement1;
                         Vector3 ba = minDistanceDisplacement2 - minDistanceDisplacement1;
 
+                        //https://www.youtube.com/watch?v=PMltMdi1Wzg
                         float weigth = Mathf.Clamp01(Vector3.Dot(pa, ba) / (Vector3.Dot(ba, ba)));
+                        
                         voronoi[x * size.y * size.z + y * size.z + z] = new Voronoi3DResult
                         {
                             displacement1 = displacement1,
                             displacement2 = displacement2,
                             weight = weigth
+                        };
+                    }
+                }
+            });
+
+            Log.Debug("Voronoi baked");
+        }
+
+        private static readonly Vector3[] _offsets = new Vector3[]
+        {
+            new Vector3(1, 0, 0),
+            new Vector3(0, 1, 0),
+            new Vector3(0, 0, 1),
+        };
+
+        [ContextMenu("BakeChebyshev")]
+        public void BakeChebyshev()
+        {
+            voronoi = new Voronoi3DResult[size.x * size.y * size.z];
+
+            Vector3 scale = new Vector3(
+                cellCounts.x / (float)size.x,
+                cellCounts.y / (float)size.y,
+                cellCounts.z / (float)size.z);
+
+            Vector3 scaleReciprocal = new Vector3(
+                1 / scale.x,
+                1 / scale.y,
+                1 / scale.z);
+
+            Parallel.For(0, size.x, x =>
+            {
+                for (int y = 0; y < size.y; y++)
+                {
+                    for (int z = 0; z < size.z; z++)
+                    {
+                        Vector3 uvw = new Vector3(
+                            x * scale.x,
+                            y * scale.y,
+                            z * scale.z);
+
+                        Vector3 uvwIntegral = new Vector3(
+                            Mathf.Floor(uvw.x),
+                            Mathf.Floor(uvw.y),
+                            Mathf.Floor(uvw.z));
+
+                        Vector3 uvwFractional = uvw - uvwIntegral;
+
+                        float minDistance = float.MaxValue;
+                        Vector3 minDisplacement = default;
+                        Vector3 minNeighborDisplacement = default;
+                        Vector3 minNeighbor = default;
+
+                        for (int i = -1; i <= 1; i++)
+                        {
+                            for (int j = -1; j <= 1; j++)
+                            {
+                                for (int k = -1; k <= 1; k++)
+                                {
+                                    Vector3 neighbor = new Vector3(i, j, k);
+                                    Vector3 pos = uvwIntegral + neighbor;
+                                    Vector3 clampedPos = new Vector3(
+                                        ((pos.x % cellCounts.x) + cellCounts.x) % cellCounts.x,
+                                        ((pos.y % cellCounts.y) + cellCounts.y) % cellCounts.y,
+                                        ((pos.z % cellCounts.z) + cellCounts.z) % cellCounts.z);
+
+                                    Vector3 displacement = RandomPG.Random3(clampedPos);
+                                    Vector3 neighborDisplacement = neighbor + displacement;
+                                    Vector3 diff = neighborDisplacement - uvwFractional;
+
+                                    float dist = Mathf.Max(Mathf.Abs(diff.x), Mathf.Abs(diff.y), Mathf.Abs(diff.z));
+
+                                    if (dist < minDistance)
+                                    {
+                                        minDisplacement = diff;
+                                        minDistance = dist;
+                                        minNeighborDisplacement = neighborDisplacement;
+                                        minNeighbor = neighbor;
+                                    }
+                                }
+                            }
+                        }
+
+                        float minWeight = float.MaxValue;
+                        Vector3 minDisplacement2 = default;
+
+                        for (int i = 0; i < 3; i++)
+                        {
+                            int offset = minDisplacement[i] < 0 ? 1 : -1;
+                            Vector3 neighbor = offset * _offsets[i];
+                            Vector3 pos = uvwIntegral + minNeighbor + neighbor;
+                            Vector3 clampedPos = new Vector3(
+                                ((pos.x % cellCounts.x) + cellCounts.x) % cellCounts.x,
+                                ((pos.y % cellCounts.y) + cellCounts.y) % cellCounts.y,
+                                ((pos.z % cellCounts.z) + cellCounts.z) % cellCounts.z);
+
+                            Vector3 displacement = RandomPG.Random3(clampedPos);
+                            Vector3 neighborDisplacement = neighbor + minNeighbor + displacement;
+                            Vector3 diff = neighborDisplacement - minNeighborDisplacement;
+
+                            int maxAxisIndex = i;
+                            //int maxAxisIndex = 0;
+                            //float maxAxisDistance = float.MinValue;
+                            //
+                            //for (int j = 0; j < 3; j++)
+                            //{
+                            //    float axisDistance = Mathf.Abs(diff[j]);
+                            //    if (axisDistance > maxAxisDistance)
+                            //    {
+                            //        maxAxisIndex = j;
+                            //        maxAxisDistance = axisDistance;
+                            //    }
+                            //}
+
+                            //float axisMinValue = Mathf.Min(neighborDisplacement[maxAxisIndex], minNeighborDisplacement[maxAxisIndex]); 
+                            //float axisMaxValue = Mathf.Max(neighborDisplacement[maxAxisIndex], minNeighborDisplacement[maxAxisIndex]); 
+                            
+                            float min = minDisplacement[i] < 0 ? neighborDisplacement[i] : minNeighborDisplacement[i];
+                            float max = minDisplacement[i] < 0 ? minNeighborDisplacement[i] : neighborDisplacement[i];
+
+                            float weigth = Mathf.InverseLerp(max, min, uvwFractional[maxAxisIndex]);
+
+                            if (weigth > 0.5f)
+                            {
+                                weigth = 1 - weigth;
+                            }
+
+                            if (weigth < minWeight)
+                            {
+                                minWeight = weigth;
+                                minDisplacement2 = neighborDisplacement - uvwFractional;
+                            }
+                        }
+
+                        Vector3 displacement1 = new Vector3(
+                            scaleReciprocal.x * minDisplacement.x,
+                            scaleReciprocal.y * minDisplacement.y,
+                            scaleReciprocal.z * minDisplacement.z);
+
+                        Vector3 displacement2 = new Vector3(
+                            scaleReciprocal.x * minDisplacement2.x,
+                            scaleReciprocal.y * minDisplacement2.y,
+                            scaleReciprocal.z * minDisplacement2.z);
+
+                        /*
+                        
+                        Vector3 pa = -minDisplacement;
+                        Vector3 ba = minDistanceDisplacement2 - minDisplacement;
+
+                        float weigth;
+                        if (metric == Metric.Euclidean)
+                        {
+                            //https://www.youtube.com/watch?v=PMltMdi1Wzg
+                            minWeigth = Mathf.Clamp01(Vector3.Dot(pa, ba) / (Vector3.Dot(ba, ba)));
+                        }
+                        else
+                        {
+                            float deltaX = Mathf.Abs(ba.x);
+                            float deltaY = Mathf.Abs(ba.y);
+                            float deltaZ = Mathf.Abs(ba.z);
+
+                            float deltaMax = Mathf.Max(deltaX, deltaY, deltaZ);
+
+                            if (deltaX == deltaMax)
+                            {
+                                minWeigth = Mathf.Clamp01(Mathf.Abs(pa.x) / deltaX);
+                            }
+                            else if (deltaY == deltaMax)
+                            {
+                                minWeigth = Mathf.Clamp01(Mathf.Abs(pa.y) / deltaY);
+                            }
+                            else
+                            {
+                                minWeigth = Mathf.Clamp01(Mathf.Abs(pa.y) / deltaZ);
+                            }
+
+                            //float h = Mathf.Clamp01(Vector3.Dot(pa, ba) / (Vector3.Dot(ba, ba)));
+                            //Vector3 q = ba.normalized * h;
+                            ////weigth = Mathf.Max(Mathf.Abs(v.x), Mathf.Abs(v.y), Mathf.Abs(v.z));
+                            //
+                            //weigth = Mathf.Max(Mathf.Abs(q.x), Mathf.Abs(q.y), Mathf.Abs(q.z))
+                            //    / Mathf.Max(Mathf.Abs(ba.x), Mathf.Abs(ba.y), Mathf.Abs(ba.z));
+                        }
+                        */
+                        voronoi[x * size.y * size.z + y * size.z + z] = new Voronoi3DResult
+                        {
+                            displacement1 = displacement1,
+                            displacement2 = displacement2,
+                            weight = minWeight
                         };
                     }
                 }
